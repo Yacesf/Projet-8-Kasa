42f348d6da7a8cb95710d03bec72d29c5d70fb79
Yace Sf <yacine.sf@outlook.fr>
2023-09-18 14:40:48 +0200

    Suppression de la pagination d'AppartmentPage et Affichage de l'errorPage dans la page AppartmentPage

--- src/pages/AppartmentPage/appartment-page.jsx
+++ src/pages/AppartmentPage/appartment-page.jsx
@@ -6,21 +6,31 @@ import Footer from "../../components/Footer/footer";
 import Flat from "./Flat/flat";
 import { useAppartmentContext } from "../../context/AppartmentContext";
 import { useParams } from "react-router-dom";
+import ErrorPage from "../ErrorPage/errorPage";
 
 function AppartmentPage() {
   const params = useParams();
   const appartmentContext = useAppartmentContext();
   const [appartmentSelected, setAppartmentSelected] = useState(null);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     if (appartmentContext.length > 0) {
       const selectedAppartment = appartmentContext.find(
         (item) => item.id === params.id
       );
-      setAppartmentSelected(selectedAppartment);
+      if (selectedAppartment) {
+        setAppartmentSelected(selectedAppartment);
+      } else {
+        setIsError(true);
+      }
     }
   }, [appartmentContext, params.id]);
   
+  if (isError) {
+    return <ErrorPage />;
+  }
+
   return (
     <div>
       <Container>
